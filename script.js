e3aa8c1ed9d168f538bee3452d34346a075b78f1
chivas911 <103080269+chivas911@users.noreply.github.com>
2025-06-15 23:31:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function(){
   $(".twentytwenty-container").twentytwenty({
-    default_offset_pct: 0.5, // Middle
+    default_offset_pct: 0.5,
     orientation: 'horizontal',
     no_overlay: true,
     move_with_handle_only: true,
